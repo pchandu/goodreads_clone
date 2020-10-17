661bfb3c562bc01a311f2a36b27c7d18850a949c
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-16 21:19:54 -0400

    fixed up routing issues so random urls will be redirected to home

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import SignupContainer from './session/signup_container'
-import { Route } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import NavbarContainer from './nav_bar/nav_bar_container'
 import LoginContainer from './session/login_container'
 import SplashHomeContainer from './home/splash_home_container'
@@ -10,9 +10,13 @@ export default () => {
     return (
     <div>
         <Route path="/" component={NavbarContainer} />
-        <Route exact path="/" component={SplashHomeContainer} />
-        <AuthRoute path="/signup" component={SignupContainer} />
-        <AuthRoute path="/login" component={LoginContainer} />
+        <Switch>
+            <AuthRoute path="/signup" component={SignupContainer} />
+            <AuthRoute path="/login" component={LoginContainer} />
+            <Route exact path="/" component={SplashHomeContainer} />
+            <Route component={SplashHomeContainer} /> 
+            {/* ^^ this could be a 404 page */}
+        </Switch>
     </div>
     )
 }

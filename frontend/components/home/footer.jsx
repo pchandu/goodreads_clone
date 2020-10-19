21cf151915980d01c45a1c40cd0e3583e2147a67
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-19 16:38:52 -0700

    updated footer links

--- frontend/components/home/footer.jsx
+++ frontend/components/home/footer.jsx
@@ -12,19 +12,21 @@ class Footer extends React.Component {
                     <br></br>
                     <a href="">Website</a>
                     <br></br>
-                    <a href="">Email</a>
+                    <a href="praneethchandu@gmail.com">Email</a>
                     <br></br>
                 </div>
                 
                 <div className="footer-column2">
                     <h2>Books</h2>
-                    <a href="">Best Seller</a>
+                    <a href="https://www.amazon.com/Meditations-New-Translation-Marcus-Aurelius/dp/0812968255/ref=zg_bs_468230_1?_encoding=UTF8&psc=1&refRID=Y7R5RMKGKSSDW8FGGAP6">
+                        Best Seller</a>
                     <br></br>
-                    <a href="">2020 Favorite</a>
+                    <a href="https://www.amazon.com/dp/1328663817?tag=fivebooks001-20">
+                        2020 Favorite</a>
                     <br></br>
-                    <a href="">Classic</a>
+                    <a href="https://www.amazon.com/Guns-Germs-Steel-Fates-Societies/dp/0393317552">Classic</a>
                     <br></br>
-                    <a href="">Developer's Choice</a>
+                    <a href="https://www.amazon.com/Sapiens-Humankind-Yuval-Noah-Harari/dp/0062316095">Developer's Choice</a>
                     <br></br>
                 </div>
             </div>

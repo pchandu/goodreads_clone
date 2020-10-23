a3ecd73e2ad5683bd94d0a2a325513567345d80d
Praneeth Chandu <praneethchandu@gmail.com>
2020-10-23 00:36:47 -0700

    couple more touches on home

--- frontend/components/home/main_content.jsx
+++ frontend/components/home/main_content.jsx
@@ -16,9 +16,10 @@ class MainContent extends React.Component {
     render(){
         const {books} = this.props
         var joseDiscovered;
-        const joseLiked = Object.entries(books).length > 0 ? 
+        const splashBooks = Object.entries(books).length > 0 ? 
             Object.values(books).map((book, i) => {
                 {
+                    // while(i > 14)
                     return (
                         <div className="book-index-item" key={i}>
                             <a href={`#/books/${book.id}`}>
@@ -37,44 +38,21 @@ class MainContent extends React.Component {
                     <div className='main-content-head-1'>
                         <h2>Deciding what to read next?</h2>
                             <p>You’re in the right place. Tell us what titles or genres you’ve enjoyed in the past, and we’ll give you surprisingly insightful recommendations.</p>
-                        </div>
-                        <div className ='main-content-head-2'>
-                        <h2>What are your friends reading?</h2>
-                            <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.</p>
-                        </div>
                     </div>
                     
-                    <div className="discovery-box">
-                            <h2>What will you discover?</h2>
-                        <div className='discovery-examples'>
-                        <div className="jose-example">
-                            <div className='jose-liked'>
-                                <h3>because Jose liked..</h3>
-                                <div className="jose-liked-books">
-                                    {joseLiked}
-                                </div>
-                            </div>
-                            
-                            <div className='jose-discovery'>
-                                <h3>he discovered:</h3>
-                                <div className="jose-discovered-books">
-                                    {/* {joseDiscovered} */}
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div className="brandon-example">
-                            <div className='brandon-liked'>
-                                <h3>because Brandon liked..</h3>
-                            </div>
-                            
-                            <div className='brandon-discovery'>
-                                <h3>he discovered:</h3>
-                            </div>
-                        </div>
+                    <div className ='main-content-head-2'>
+                        <h2>What are your friends reading?</h2>
+                            <p>Chances are your friends are discussing their favorite (and least favorite) books on Goodreads.</p>
                     </div>
+            </div>
 
+            <div className="discovery-box">
+                <h2>What will you discover?</h2>
+                <div className="splash-book-covers">
+                    {splashBooks}
                 </div>
+            </div>
+
             </div>
         )
     }
